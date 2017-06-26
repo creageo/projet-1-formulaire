94d480120261069850ee7e856305dc0ae3c954dc
creageo <geoffreymarique@gmail.com>
2017-06-26 12:59:29 +0200

    Add files via upload

--- reponse.php
+++ reponse.php
@@ -5,7 +5,16 @@ echo($_POST["prenom"]);
 echo "</br>";
 echo($_POST["nom"]);
 echo "</br>";
-echo($_POST["email"]);
+
+//validation
+$emailIsValid = filter_var($_POST["email"], FILTER_VALIDATE_EMAIL);
+
+if(!empty($emailIsValid)){
+
+     echo($emailIsValid);
+
+}
+
 echo "</br>";
 echo($_POST["pays"]);
 echo "</br>";
@@ -13,18 +22,27 @@ echo($_POST["genre"]);
 echo "</br>";
 echo($_POST["sujet"]);
 echo "</br>";
-echo($_POST["message"]);
 
-               $to = $_POST["email"];
-               $subject = 'Mail pour le support technique';
-               $headers = 'Mime-Version: 1.0'."\r\n";
-               $headers .= 'Content-type: text/html; charset=utf-8'."\r\n";
-               $headers .= "\r\n";
-               $message  = 'De: ' . $_POST["nom"] . $_POST["prenom"] . "<br />";
-               $message .= 'Email: ' . $_POST["email"] . "<br />";
-               $message .= 'Type de probleme: ' . $_POST["sujet"] . "<br />";
-               $message .= "Message:<br />" . $_POST["message"] . "<br />";
-               $envoi = mail($to, $subject, $message, $headers);
+//sanitization
+$tachesani = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+
+//validation
+if(!empty($tachesani)){
+
+     echo($tachesani);
+
+}
+
+$to = $_POST["email"];
+$subject = 'Mail pour le support technique';
+$headers = 'Mime-Version: 1.0'."\r\n";
+$headers .= 'Content-type: text/html; charset=utf-8'."\r\n";
+$headers .= "\r\n";
+$message  = 'De: ' . $_POST["nom"] . $_POST["prenom"] . "<br />";
+$message .= 'Email: ' . $_POST["email"] . "<br />";
+$message .= 'Type de probleme: ' . $_POST["sujet"] . "<br />";
+$message .= "Message:<br />" . $_POST["message"] . "<br />";
+$envoi = mail($to, $subject, $message, $headers);
 
 ?>
 </pre>
