3b74b74d30505af536c68990bc64b76eac18d4d0
creageo <geoffreymarique@gmail.com>
2017-06-26 16:02:35 +0200

    Add files via upload

--- reponse.php
+++ reponse.php
@@ -1,14 +1,20 @@
 <pre>
+
 <?php
 
+//afficher les informations du formulaire
 echo($_POST["prenom"]);
-echo "</br>";
-echo($_POST["nom"]);
+
 echo "</br>";
 
-//validation
+echo($_POST["nom"]);
+
+echo "</br>";
+
+//validation de l'email
 $emailIsValid = filter_var($_POST["email"], FILTER_VALIDATE_EMAIL);
 
+//validation de l'email et l'afficher
 if(!empty($emailIsValid)){
 
      echo($emailIsValid);
@@ -16,33 +22,50 @@ if(!empty($emailIsValid)){
 }
 
 echo "</br>";
+
 echo($_POST["pays"]);
-echo "</br>";
-echo($_POST["genre"]);
-echo "</br>";
-echo($_POST["sujet"]);
+
 echo "</br>";
 
-//sanitization
+echo($_POST["genre"]);
+
+echo "</br>";
+
+echo($_POST["sujet"]);
+
+echo "</br>";
+
+//sanitization du message
 $tachesani = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
 
-//validation
+//validation du message et l'afficher
 if(!empty($tachesani)){
 
      echo($tachesani);
 
 }
 
+//envoie des informations dans l'email indiquer par l'utilisateur
 $to = $_POST["email"];
+
 $subject = 'Mail pour le support technique';
+
 $headers = 'Mime-Version: 1.0'."\r\n";
+
 $headers .= 'Content-type: text/html; charset=utf-8'."\r\n";
+
 $headers .= "\r\n";
+
 $message  = 'De: ' . $_POST["nom"] . $_POST["prenom"] . "<br />";
+
 $message .= 'Email: ' . $_POST["email"] . "<br />";
+
 $message .= 'Type de probleme: ' . $_POST["sujet"] . "<br />";
+
 $message .= "Message:<br />" . $_POST["message"] . "<br />";
+
 $envoi = mail($to, $subject, $message, $headers);
 
 ?>
+
 </pre>
